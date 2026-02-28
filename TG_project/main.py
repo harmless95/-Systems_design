0805cad88b1c13f09a9302a7933c9f485255d970
harmles95 <213446016+harmless95@users.noreply.github.com>
2026-02-28 19:40:53 +0700

    We've fixed data sending in the Telegram project.

--- TG_project/main.py
+++ TG_project/main.py
@@ -2,7 +2,6 @@ import asyncio
 import os
 import httpx
 import logging
-import json
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
@@ -18,20 +17,24 @@ dp = Dispatcher()
 
 
 async def handler_message(message: Message):
-    data = message.json()
-    data_json = json.loads(data)
+
+    data_json = message.model_dump()
     logger.error("Data TG: %s, %s", type(data_json), data_json)
     try:
-        async with httpx.AsyncClient() as client:
+        async with httpx.AsyncClient(timeout=30.0) as client:
             response = await client.post(url=URL_APP, json=data_json)
             logger.error("Resp: %s", response)
             if response.status_code == 200:
-                return True
-            else:
-                logger.warning(f"Приложение вернуло ошибку: {response.status_code}")
-                return False
+                return "Сообщение обработанно"
+
+            logger.warning("API Error %s: %s", response.status_code, response.text)
+            return "Не удалось обработать данные"
+
+    except httpx.ReadTimeout:
+        return "Сервер слишком долго думал, но мы продолжаем обработку"
     except Exception as ex:
-        logger.error("Error sending webhook: %s", ex)
+        logger.exception("Webhook sending failed")
+        return "Ошибка связи с сервером"
 
 
 @dp.message(CommandStart())
@@ -46,10 +49,7 @@ async def get_start(message: Message):
 async def message_text(message: Message):
     await message.answer("Сообщение принято")
     result_app = await handler_message(message=message)
-    if result_app:
-        await message.answer("Сообщение обработанно")
-    else:
-        await message.answer("Произошла ошибка при связи с сервером")
+    await message.answer(result_app)
 
 
 async def main():
